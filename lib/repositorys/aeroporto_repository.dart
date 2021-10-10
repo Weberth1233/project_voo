0416cc4c54c534e6e9ecd648ca3bcb98ed483592
Your Name <you@example.com>
2021-10-09 21:13:12 -0300

    Adicionando aeroportos na listagem

---
+++ lib/repositorys/aeroporto_repository.dart
@@ -0,0 +1,11 @@
+import 'package:project_decolar/models/aeroporto.dart';
+
+class AeroportoRepository {
+  static List<Aeroporto> tabela = [
+    Aeroporto("Aeroporto Brigadeiro Lysias Rodrigues","Av. Joaquim Teotônio Segurado, s/n - Plano Diretor Expansão Sul, Palmas - TO, "
+        "77061-900", -10.295667, -48.358156),
+    Aeroporto("Aeroporto de Paraíso do Tocantins","Rodovia, TO-447 - St. Oeste, Paraíso do Tocantins - TO, 77600-000",
+        -10.1760978, -48.9341201),
+    Aeroporto("Aeroporto de Porto Nacional","Vila Militar, Porto Nacional - TO, 77500-000", -10.4348271, -48.6318038),
+  ];
+}
