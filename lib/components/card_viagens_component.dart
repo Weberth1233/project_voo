5cc8df484d9a81dd36c5cd1953d266cb6e61a14c
Your Name <you@example.com>
2021-10-09 21:12:36 -0300

    Alterando card de viagens

--- lib/components/card_viagens_component.dart
+++ lib/components/card_viagens_component.dart
@@ -12,89 +12,91 @@ class CardViagens extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Card(
-        clipBehavior: Clip.antiAlias,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  Ink.image(
-                    image: const NetworkImage(
-                        'https://cdn.pixabay.com/photo/2020/06/23/11/07/transport-5332224_1280.jpg'),
-                    height: 240,
-                    child: InkWell(
-                      onTap: () {},
-                    ),
-                    fit: BoxFit.cover,
+    String aviao = companhiaViagem.aviao;
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const ListTile(
+              title: Text('VOOS', style: TextStyle(fontWeight: FontWeight.bold),),
+              subtitle: Text('Informações sobre o voo'),
+              trailing: Icon(Icons.favorite_outline),
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Ink.image(
+                  image: const NetworkImage(
+                      'https://cdn.pixabay.com/photo/2020/06/23/11/07/transport-5332224_1280.jpg'),
+                  height: 240,
+                  child: InkWell(
+                    onTap: () {},
                   ),
-                  Text(
-                    companhiaViagem.aviao,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
-                  ),
-                ],
-              ),
-
-              Text('Informações Viagem', style: TextStyle(fontSize: 15),),
-              Divider(),
-              Row(
-                children: [
-                  const Icon(Icons.map),
-                  Text(
-                    'Destino:' + companhiaViagem.destino.nome,
-                    style: const TextStyle(fontSize: 12),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  const Icon(Icons.my_location),
-                  Text(
-                    'Origem:' + companhiaViagem.origem.nome,
-                    style: const TextStyle(fontSize: 12),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  const Icon(Icons.calendar_today),
-                  Text(
-                    'Data inicial: ' + companhiaViagem.getFormatDateInicial(),
-                    style: const TextStyle(fontSize: 12),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  const Icon(Icons.date_range),
-                  Text(
-                    'Data final: ' + companhiaViagem.getFormatDateFinal(),
-                    style: const TextStyle(fontSize: 12),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Align(
-                  alignment: Alignment.topRight,
-                  child: Text(
-                    companhiaViagem.getCurrency(),
-                    style: const TextStyle(
-                        fontSize: 17, fontWeight: FontWeight.bold),
+                  fit: BoxFit.cover,
+                ),
+                Text(
+                  companhiaViagem.aviao,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 24,
                   ),
                 ),
+              ],
+            ),
+            Divider(),
+            Row(
+              children: [
+                const Icon(Icons.map),
+                Text(
+                  'Destino:' + companhiaViagem.destino.nome,
+                  style: const TextStyle(fontSize: 12),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                const Icon(Icons.my_location),
+                Text(
+                  'Origem:' + companhiaViagem.origem.nome,
+                  style: const TextStyle(fontSize: 12),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                const Icon(Icons.calendar_today),
+                Text(
+                  'Data inicial: ' + companhiaViagem.getFormatDateInicial(),
+                  style: const TextStyle(fontSize: 12),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                const Icon(Icons.date_range),
+                Text(
+                  'Data final: ' + companhiaViagem.getFormatDateFinal(),
+                  style: const TextStyle(fontSize: 12),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Text(
+                  companhiaViagem.getCurrency(),
+                  style: const TextStyle(
+                      fontSize: 17, fontWeight: FontWeight.bold),
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
